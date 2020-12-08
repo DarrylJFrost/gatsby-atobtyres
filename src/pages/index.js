fe286abefea9cfd77f37c704a5c2d1e8c257e422
DarrylJFrost <darryljf.develop@gmail.com>
2020-12-08 15:18:42 +0000

    dart sass package installed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
+import { faDivide } from "@fortawesome/free-solid-svg-icons"
 import React from "react"
+import styles from '../styles/test.module.scss'
 
-
-
+const IndexPage = () => {
+  return (
+    <div className={styles.test}>
+      <h1>IndexPage</h1>
+    </div>
+    
+  )
 }
+
+export default IndexPage
+
+
