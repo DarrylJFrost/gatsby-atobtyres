b749e35be7cc1323fbf0a6eded494f89ceb95e6e
DJF <darryljf.develop@gmail.com>
2020-12-11 12:45:24 +0000

    got questions component and stylings added

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -18,19 +18,22 @@ const Questions = () => {
       <div className={styles.contactCenter}>
         <Row className={`${styles.contactInfo} justify-content-center`}>
           <Col sm={12}>
-            <h4>
+            <h4 className="mb-4">
               <span className={styles.marks}>//</span>
-              Got Questions?
+              got questions?
             </h4>
           </Col>
           <Col md={4} lg={3}>
             <FontAwesomeIcon icon={faPhone}/>
+            <a href="tel:+441473269030">01473 269030</a>
           </Col>
           <Col md={4} lg={3}>
             <FontAwesomeIcon icon={faEnvelope}/>
+            <a href="mailto:BSMYTH@HOTMAIL.CO.UK">BSMYTHE@HOTMAIL.CO.UK</a>
           </Col>
           <Col md={4} lg={3}>
             <FontAwesomeIcon icon={faFacebookF}/>
+            <a href="https://facebook.com/atobtyres" target="_blank" rel="noopener noreferrer">FACEBOOK</a>
           </Col>
         </Row>
       </div>
